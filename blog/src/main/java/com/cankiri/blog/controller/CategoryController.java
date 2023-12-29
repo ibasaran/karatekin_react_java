2e0f9b30b90a9864ef33633d919250be3d91f262
ismail BAŞARAN <basaran.ismaill@gmail.com>
2023-12-29 11:12:35 +0300

    category rest

--- blog/src/main/java/com/cankiri/blog/controller/CategoryController.java
+++ blog/src/main/java/com/cankiri/blog/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.cankiri.blog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,20 +13,36 @@ import com.cankiri.blog.model.Category;
 import com.cankiri.blog.repository.CategoryRepository;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/category")
 public class CategoryController {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	@RequestMapping("/categories")
+	@RequestMapping("/list")
 	public List<Category> getCategories() {	
 		return categoryRepository.findAll();
 	}
 	
-	@RequestMapping(value = "/category/create", method = RequestMethod.POST)
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public Category createCategory(@RequestBody Category category) {
 		return categoryRepository.save(category);
 	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public Category update(@RequestBody Category category) {
+		return categoryRepository.save(category);
+	}
+	
+	@RequestMapping(value = "/detail/{id}")
+	public Category getDetail(@PathVariable Long id) {
+		return categoryRepository.findById(id).get();
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id) {
+		categoryRepository.deleteById(id);
+	}
+	
 
 }
